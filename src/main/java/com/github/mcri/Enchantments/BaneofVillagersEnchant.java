bda147b76472ab2f0bb3fc746a4e9247b8b3402a
Winterblue3D <winterbluetv@gmail.com>
2022-08-30 01:23:18 -0700

    implemented #237

--- src/main/java/com/github/mcri/Enchantments/BaneofVillagersEnchant.java
+++ src/main/java/com/github/mcri/Enchantments/BaneofVillagersEnchant.java
@@ -53,12 +53,12 @@ public class BaneofVillagersEnchant extends Enchantment {
 
     @Override
     public int getMinPower(int level) {
-        return 1;
+        return level * 6;
     }
 
     @Override
     public int getMaxPower(int level) {
-        return level * 6;
+        return this.getMinPower(level) + 5;
     }
 
     @Override
