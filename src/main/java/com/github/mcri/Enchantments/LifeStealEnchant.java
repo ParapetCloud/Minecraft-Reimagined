cd287ea7986eb18b9d7f160fc8aa8c733dfef8c1
Parapet Cloud <2636908+ParapetCloud@users.noreply.github.com>
2022-08-29 17:36:32 -0700

    expanded enchantments to include axes and tridents (#213)

--- src/main/java/com/github/mcri/Enchantments/LifeStealEnchant.java
+++ src/main/java/com/github/mcri/Enchantments/LifeStealEnchant.java
@@ -8,6 +8,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.TridentItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -20,6 +24,15 @@ public class LifeStealEnchant extends Enchantment {
 
     public LifeStealEnchant() {
         super(Enchantment.Rarity.VERY_RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[] { EquipmentSlot.MAINHAND });
+    }
+
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        Item item = stack.getItem();
+        if (item instanceof AxeItem || item instanceof TridentItem) {
+            return true;
+        }
+        return super.isAcceptableItem(stack);
     }
 
     @Override
