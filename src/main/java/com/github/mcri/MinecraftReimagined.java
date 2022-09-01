884646f99638f701730fe7d62b630ac0bf9ea7b3
Winterblue <95664414+Winterblue3D@users.noreply.github.com>
2022-08-31 20:31:52 -0700

    skies enchantments (#304)
    * added all "skies" enchants, moved MOD_ID variable up so it can be used in the getLogger() method
    
    * implemented #224
    
    * implemented #226
    
    * implemented #227
    
    * implemented #225
    
    * added "skies" enchants to language file
    
    * fixed #224

--- src/main/java/com/github/mcri/MinecraftReimagined.java
+++ src/main/java/com/github/mcri/MinecraftReimagined.java
@@ -13,9 +13,10 @@ public class MinecraftReimagined implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("mcri");
-
 	public static final String MOD_ID = "mcri";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 	@Override
 	public void onInitialize() {
@@ -38,6 +39,10 @@ public class MinecraftReimagined implements ModInitializer {
 		BaneofSwinesEnchant.Register();
 		BaneofVillagersEnchant.Register();
 		BaneofIllagersEnchant.Register();
+		ClearSkiesEnchant.Register();
+		NoSkiesEnchant.Register();
+		DarkSkiesEnchant.Register();
+		StormySkiesEnchant.Register();
 
 		// Status effects
 		FrozenEffect.Register();
