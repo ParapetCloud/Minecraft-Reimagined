7fa30da1c648081228dfcb529027aca456c564cc
Winterblue <95664414+Winterblue3D@users.noreply.github.com>
2022-09-01 22:25:50 -0700

    implemented #196 (#329)

---
+++ src/main/java/com/github/mcri/Mixins/ElytraDisableFireworksMixin.java
@@ -0,0 +1,17 @@
+package com.github.mcri.Mixins;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
+import net.minecraft.util.math.Vec3d;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FireworkRocketEntity.class)
+public abstract class ElytraDisableFireworksMixin {
+    @Redirect(method = { "tick" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
+    private void elytraFireworkDisable(LivingEntity instance, Vec3d vec3d) {
+
+    }
+}
