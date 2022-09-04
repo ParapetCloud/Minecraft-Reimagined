4502b9d1834b9448cc60602641a41416d993ce52
Winterblue3D <winterbluetv@gmail.com>
2022-09-03 18:36:36 -0700

    deleted unnecessary block class files

--- src/main/java/com/github/mcri/block/ModBlocks.java
+++ src/main/java/com/github/mcri/block/ModBlocks.java
@@ -7,47 +7,50 @@ import com.github.mcri.utils.BlockItemSettings.Group;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.WallBlock;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
 
 public final class ModBlocks {
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final DirtSlab DIRT_SLAB = new DirtSlab(
-        FabricBlockSettings.of(Material.SOIL).strength(0.5f).sounds(BlockSoundGroup.GRAVEL));
+    public static final SlabBlock DIRT_SLAB = new SlabBlock(
+            FabricBlockSettings.of(Material.SOIL).strength(0.5f).sounds(BlockSoundGroup.GRAVEL));
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final DirtyCobblestone DIRTY_COBBLESTONE = new DirtyCobblestone(
-        FabricBlockSettings.of(Material.STONE).strength(1.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block DIRTY_COBBLESTONE = new Block(
+            FabricBlockSettings.of(Material.STONE).strength(1.0f).sounds(BlockSoundGroup.STONE).requiresTool());
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final DirtyCobblestoneSlab DIRTY_COBBLESTONE_SLAB = new DirtyCobblestoneSlab(
-        FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final SlabBlock DIRTY_COBBLESTONE_SLAB = new SlabBlock(
+            FabricBlockSettings.copy(DIRTY_COBBLESTONE));
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final DirtyCobblestoneStairs DIRTY_COBBLESTONE_STAIRS = new DirtyCobblestoneStairs(
-        DIRTY_COBBLESTONE.getDefaultState(),
-        FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final StairsBlock DIRTY_COBBLESTONE_STAIRS = new StairsBlock(
+            DIRTY_COBBLESTONE.getDefaultState(),
+            FabricBlockSettings.copy(DIRTY_COBBLESTONE));
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final DirtyCobblestoneWall DIRTY_COBBLESTONE_WALL = new DirtyCobblestoneWall(
-        FabricBlockSettings.copy(DIRTY_COBBLESTONE).requiresTool());
+    public static final WallBlock DIRTY_COBBLESTONE_WALL = new WallBlock(
+            FabricBlockSettings.copy(DIRTY_COBBLESTONE).requiresTool());
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final SnowyCobblestone SNOWY_COBBLESTONE = new SnowyCobblestone(
-        FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block SNOWY_COBBLESTONE = new Block(
+            FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final SnowyCobblestoneSlab SNOWY_COBBLESTONE_SLAB = new SnowyCobblestoneSlab(
-        FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final SlabBlock SNOWY_COBBLESTONE_SLAB = new SlabBlock(
+            FabricBlockSettings.copy(SNOWY_COBBLESTONE));
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final SnowyCobblestoneStairs SNOWY_COBBLESTONE_STAIRS = new SnowyCobblestoneStairs(
-        SNOWY_COBBLESTONE.getDefaultState(),
-        FabricBlockSettings.of(Material.STONE).strength(2.0f).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final StairsBlock SNOWY_COBBLESTONE_STAIRS = new StairsBlock(
+            SNOWY_COBBLESTONE.getDefaultState(),
+            FabricBlockSettings.copy(SNOWY_COBBLESTONE));
 
     @BlockItemSettings(group = Group.BUILDING_BLOCKS)
-    public static final SnowyCobblestoneWall SNOWY_COBBLESTONE_WALL = new SnowyCobblestoneWall(
-        FabricBlockSettings.copy(DIRTY_COBBLESTONE).requiresTool());
+    public static final WallBlock SNOWY_COBBLESTONE_WALL = new WallBlock(
+            FabricBlockSettings.copy(SNOWY_COBBLESTONE));
 
     /**
      * Registers all the effects that have been pre-registered
